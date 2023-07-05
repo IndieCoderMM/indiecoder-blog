ff573ca538e1f680fe2a1cb099fda596e2c79d61
hein thant <hthant00chk@gmail.com>
2023-07-05 23:46:56 +0630

    Fix typos

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <section className="grid bg-white grid-cols-3 items-center justify-items-center p-3">
       <div className="col-span-2 text-lg p-5">
         <h1 className="font-bold text-5xl mb-3">Welcome to My Blog!</h1>
-        <h3 className="font-bold text-xl mb-5">Hello, I'm Indie Coder!</h3>
+        <h3 className="font-bold text-xl mb-5">Hello, I&apos;m Indie Coder!</h3>
         <p className="text-md">
-          As a web developer based in Myanmar, I'm passionate about sharing my
-          knowledge through articles, tutorials, and fun projects. I believe
+          As a web developer based in Myanmar, I&apos;m passionate about sharing
+          my knowledge through articles, tutorials, and fun projects. I believe
           that programming can be an exciting and playful experience, which is
           why I enjoy gamifying the learning process. Join me as we explore the
           world of programming together, unlocking new skills, and embracing the
